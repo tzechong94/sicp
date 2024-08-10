d78040d31b98fe989a455b89083cd5044fffc956
tc94 <tze.chong1994@gmail.com>
2024-08-10 21:57:56 +0800

    map over trees

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ fermat's little theorem
 - if n is a prime number and a is any positive integer less than n, then a raised to the nth power is congruent to a modulo n
 
 congruent modulo n if they both have the same remainder when divided by n.
+
+## Data abstraction
+
+- structure programs that are to use compound data objects so that they operate on 'abstract data'
+- seelctors and constructors to implement abstract data in terms of the concrete representation
+- concrete data representation is defined independent of the programs that use the data
+
+### Pairs
+
+- use cons. cons takes two arguments, returns a compound data object that contains two arguments as parts. given a pair, we can extract the parts using the primitive procedure car and cdr. 
+
+- cons provide primitive glue to construct compound data objects
+- box and pointer notation
+
--- run.scm
+++ run.scm
@@ -198,3 +198,388 @@
 
 ;Miller-Rabin test cannot be fooled
 ;“Numbers that fool the Fermat test are called Carmichael numbers,”
+
+;1.29 simpson's rule
+
+(define (cube x)
+    (* x x x))
+
+
+(define (inc n)
+    (+ n 1))
+
+(define (simpson f a b n)
+    (define h (/ (- b a) n))
+    (define (yk k)
+        f (+ a (* h k)))
+    (define (simpson-term k)
+        (* (cond (or (= k 0) (= k n)) 1)
+            ((odd? k) 4)
+            (else 2))
+            (yk k))
+    (* (/ h 3) (sum simpson-term 0 inc n))
+)
+
+(define (sum term a next b)
+    (if (> a b)
+    0
+    (+ (term a) (sum term (next a) next b))
+))
+
+(define (sum term a next b)
+    (define (iter a result)
+        (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))
+    ))
+    (iter a 0))
+
+(define (product term a next b)
+    (if (> a b)
+    1
+    (* (term a) (product term (next a) next b))))
+
+(define (product term a next b)
+    (define (iter a results)
+        (if (> a b)
+        result)
+        (iter (next a) (* result (term a))))
+    (iter a 1))
+
+
+(define (accumulate combiner null-value term a next b)
+    (if (a > b) (null-value)
+
+    (combiner (term a) (accumulate combiner null-value (next a) next b)))
+    )
+
+(define (accumulate combiner null-value term a next b)
+    (define (iter a result)
+        (if (> a b)
+        result
+        )
+        (iter (next a) (combiner result (term a)))
+        )
+        (iter a null-value))
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+    (if (a > b) (null-value)
+    (if (filter a)
+        (combiner (term a) (filtered-accumulate combiner null-value (next a) next b))))
+        (combiner null-value (filtered-accumulate combiner null-value term (next a) next b filter)
+    ))
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+    (define (iter a result)
+        (cond ((> a b) result)
+        ((filter a) (iter (next a) (combiner result (term a)))
+        (else (iter (next a) (result)))
+        )
+        (iter a null-value))
+))
+
+; use lambda to specify anonymous procedure for binding local variables
+; body of f then becomes a single call to that procedure
+
+(define (f x y)
+    ((lambda (a b)
+     (+ (* x (square a))
+        (* y b)
+     (* a b)
+     )
+    ))
+(+ 1 (* x y))
+(- 1 y))
+
+(define (f x y)
+    (let (a ( + 1 (* x y)))
+        (b (- 1 y))
+    )
+    (+ (* x (square a))
+    (* y b)
+    (* a b))
+    )
+
+; let var1 have the value exp1 and var2 have value exp2 in body
+
+(define (fixed-point f first-guess)
+
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2))
+        tolerance)
+        )
+    (define (try guess) 
+        (let ((next (f guess)))
+          (if (close-enough? guess next)
+          next
+          (try next))  
+        )
+        )(try first-guess)
+        )
+
+; average damping
+
+(define (average-damp f)
+    (lambda (x) 
+        (average x (f x))))
+
+(define (sqrt x)
+    (fixed-point
+        (average-damp
+            (lambda (y) (/ x y))
+        ) 1.0
+    ))
+
+(define (deriv g)
+    (lambda (x)
+     (/ (- g (+ x dx)) (g x)) dx)
+    )
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+    (lambda (x)
+    (- x (/ (g x)
+    ((deriv g) x)))))
+
+
+(define (newtons-method g guess)
+    (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+    (newtons-method
+    (lambda (y)
+    (- (square y) x))
+    1.0))
+
+(define (cubic a b c)
+    (lambda (x)
+    (+ (cube x) (* a (square x)) (* b x) c)
+    ))
+
+(define (double f)
+    (lambda x)
+    (f x)
+)
+
+(define (compose f g)
+    (lambda (x))
+    (f (g x))
+    )
+
+(define x (cons 1 2))
+
+(car x) ; head
+1
+
+(cdr x) ; tail
+2
+
+(define x (cons 1 2))
+(define y (cons 3 4))
+(define z (cons x y))
+
+(car (car z)) ; 1
+(car (cdr z)) ; 3
+
+
+(define (make-rat n d)
+    (let ((g (gcd n d)))
+        (cons (/ n d)
+        (/ d g))  
+    ))
+
+
+(define (cons x y)
+    (define (dispatch m)
+        (cond ((= m 0) x)
+            ((= m 1) y)
+            
+            (else
+            (error "Argument not 0 or 1: CONS" m)))
+        
+        ) dispatch)
+
+(define (car z)
+    (z 0))
+
+(define (cdr y)
+    (z 1))
+
+(define (cons a b)
+    (* (expt 2 a) (expt 3 b)))
+
+(define (car x)
+    (define (car-iter x count)
+    (if (= 0 (remainer x 2))
+        (car-iter (/ x 2) (+ 1 count))
+        count
+    ))
+    (car-iter x 0))
+
+(define (cdr x)
+    (define (cdr-iter x count)
+        (if (= 0 (remainder 3))
+        (cdr-iter (/ x 3) (+ 1 count))
+        count
+        ))
+        (cdr-iter x 0))
+
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+    (lambda (f) (lambda (x) (f((n f) x)))))
+
+; church numerals
+
+(add-1 zero)
+(add-1 (lambda (f) (lambda (x) x)))
+
+(lambda (f) (lambda (x) (f ((lambda (f) (lambda (x) x) f) x))))
+(lambda (f) (lambda (x) (f (f x))))
+
+(define (one (lambda (f) (lambda (x) (f (f x))))))
+
+(add-1 one) ;two
+
+(cons 1 (cons 2 (cons 3 (cons 4 nil)))) ; list
+; above is the same as 
+(list 1 2 3 4)
+
+; list operations
+(define (list-ref items n)
+    (if (= n 0)
+    (car items)
+    (list-ref (cdr items)
+    (- n 1)))
+)
+
+; null checks if list is empty
+
+((define (length items)
+    (if (null? items)
+    0
+    (+ 1 (length (cdr items))))))
+
+(define (length items)
+    ((define (length-iter a count)
+        (if (null? a)
+        count
+        (
+            (length-iter (cdr a) (+ count 1))
+        ))))
+        length-iter items 0)   
+
+; append 
+
+(define (append list1 list2)
+    (if (null? list1)
+    list2
+    (cons (car list1)
+    (append (cdr list1) list2
+    )
+    )))
+
+(define (last-pair list)
+    (let ((rest (cdr items))))
+    (if (null? rest)
+        items
+        (last-pair rest))    
+    )
+
+(define (reverse list)
+    (define (iter items results)
+        (if (null? items)
+        results
+        (iter (cdr results) (cons (car items) result))
+        
+        ))
+        (iter items nil))
+
+(define (scale-list items factor)
+    (if (null? items)
+    nil
+    (cons (* (car items) factor)
+    (scale-list (cdr items) factor)
+    )
+    ))
+
+(define (map proc items)
+    (if (null? items)
+    nil
+    (cons (proc (car items))
+    (map proc (cdr items))
+    )
+    )) 
+
+(define (for-each items proc)
+    (cond ((not (null? items))
+        (proc (car items))
+        (for-each proc (cdr items))
+    ))
+    )
+
+;recursion is natural tool for dealing with tree structures
+
+(define (count-leaves list)
+    (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ count-leaves (car x))
+        (count-leaves (cdr x))
+        )
+    )
+
+)
+
+(define (deep-reverse x)
+    (if (pair? x)
+        (append (deep-reverse (cdr x))
+            (list (deep-reverse (car x)))
+        )
+    x))
+
+; mobile consists of two branches left and right
+(define (make-mobile left right)
+    (list left right))
+
+(define (make-branch length structure)
+    (list length structure))
+
+(define (left-branch mobile)
+    (car mobile))
+
+(define (right-branch mobile)
+    (car (cdr mobile)))
+
+(define (branch-length branch)
+    (car branch))
+
+(define (branch-structure branch)
+   (car (cdr branch)))
+
+; (define (total-weight mobile)
+;     (define (iter mobile result)
+;         (if (null? (right-branch mobile))
+;             result
+;             (iter (left-branch mobile) (+ result (branch-length right-branch)))
+;         ))
+;     (iter mobile 0)
+; )
+
+(define (total-weight m)
+    (if (not (pair? m))
+    m
+    (+ (branch-structure (left-branch m))
+    (branch-structure (right-branch m)))
+    ))
+
+(define (total-weight mobile)
+    (cond ((null? mobile) 0)
+        ((not (pair? mobile)) mobile)
+        (else (+ (total-weight (branch-structure (left-branch mobile)))
+                (total-weight (branch-structure (right-branch mobile)))
+        ))
+    ))
+
+; map over trees
